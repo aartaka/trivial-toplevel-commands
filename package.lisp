33f52988ac0e10b7b796d5b55e4252570c326f42
Artyom Bologov <aartaka@protonmail.com>
2024-01-08 15:17:05 +0400

    package: Export command-handler.

--- package.lisp
+++ package.lisp
@@ -9,7 +9,7 @@
   (:use :common-lisp)
   (:export #:define-command/string #:define-command/read #:define-command/eval
            #:remove-command
-           #:command-alias #:command-name)
+           #:command-alias #:command-name #:command-handler)
   (:documentation "`trivial-toplevel-commands' allows (un)defining new toplevel commands.
 
 There are three macros defining new commands:
